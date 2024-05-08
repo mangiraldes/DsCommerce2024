ab0c2a1df6432b884c93ba50520acdf09c14dd69
Manoel Giraldes <manogiraldes@gmail.com>
2024-05-08 18:04:38 -0300

    primeiro commit dscommerce

---
+++ src/main/java/com/manogiraldes/dscommerce/DscommerceApplication.java
@@ -0,0 +1,18 @@
+package com.manogiraldes.dscommerce;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("print 1 ");
+	}
+}
